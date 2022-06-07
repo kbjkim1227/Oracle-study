213c7ea854ae75d29a4624011eb3f5b83211830a
Bongjae <kbjkim@naver.com>
2022-06-07 19:49:32 +0900

    20220607업로드

---
+++ homework/0607과제.sql
@@ -0,0 +1,33 @@
+SELECT 
+PLAYER_NAME AS 선수명,
+POSITION AS 위치,
+HEIGHT AS 키,
+WEIGHT AS 몸무게
+FROM player;
+
+
+SELECT 
+PLAYER_NAME AS "선수 명",
+POSITION AS 위치,
+HEIGHT AS 키,
+WEIGHT AS 몸무게
+FROM player;
+
+
+SELECT 
+PLAYER_NAME AS 선수명,
+HEIGHT - WEIGHT AS "키-몸무게"
+FROM player;
+
+
+SELECT 
+PLAYER_NAME AS 선수명,
+ROUND (WEIGHT / ((HEIGHT / 100) * (HEIGHT / 100)), 2)
+AS BMI비만지수
+FROM player;
+
+
+SELECT 
+PLAYER_NAME ||'선수,'||HEIGHT||'cm,'||WEIGHT||'kg' AS 체격정보
+FROM player;
+
